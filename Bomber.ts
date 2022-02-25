66a4024f177e3f31656e0ed92814b8e574ed903c
dom111 <dom111@users.noreply.github.com>
2022-02-25 21:15:05 +0000

    Initial commit.

---
+++ Bomber.ts
@@ -0,0 +1,5 @@
+import { Fortifiable } from '@civ-clone/base-unit-type-fortifiable/Fortifiable';
+
+export class Bomber extends Fortifiable {}
+
+export default Bomber;
---
+++ README.md
@@ -0,0 +1,4 @@
+# base-unit-bomber
+
+This is a template repo.
+
